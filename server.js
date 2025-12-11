095e22e92c99bba5e30f7dde6868d87f58b9cd73
ahmad2009-veb <ahmaddeveloper202@gmail.com>
2025-12-11 23:47:38 +0500

    last commit

--- server.js
+++ server.js
@@ -1,28 +1,59 @@
-const express = require('express');
-
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin:"*",
-}))
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-let items = [
-    {id:1,name:"Ahmad",age:16},
-    {id:2,name:"Muhammad",age:18},
-    {id:3,name:"Yusuf",age:12},
-]
+// ===== Connect to MongoDB =====
+mongoose
+  .connect(
+    "mongodb+srv://ahmad:A2009developer@cluster0.puzjbog.mongodb.net/?appName=Cluster0"
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error:", err));
 
-app.get('/items', (req,res) => {
-    res.json(items)
-})
+// ===== Schema & Model =====
+const itemSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
 
+const Item = mongoose.model("Item", itemSchema);
 
+// ===== Routes =====
+
+// GET
+app.get("/items", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+// POST
+app.post("/items", async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.json(newItem);
+});
+
+// DELETE
+app.delete("/items/:id", async (req, res) => {
+  await Item.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+// PUT
+app.put("/items/:id", async (req, res) => {
+  const updated = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(updated);
+});
+
+// LISTEN
 app.listen(PORT, () => {
-    console.log(`Server runnig on port ${PORT}`);  
-})
+  console.log(`Server running on port ${PORT}`);
+});
